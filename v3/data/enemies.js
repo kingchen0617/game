2c1266462bc85c73fcea3d9635ccf38e18592bc7
kingchen0617 <a0980192456@gmail.com>
2026-04-09 12:02:52 +0800

    Add v3 enemies

---
+++ v3/data/enemies.js
@@ -0,0 +1,30 @@
+const enemies = {
+  bandit: {
+    name: '荒野掠奪者',
+    level: 3,
+    hp: 60,
+    atk: 10,
+    intro: '一群掠奪者試圖圍攻你。'
+  },
+  wolf_general: {
+    name: '血狼將軍',
+    level: 8,
+    hp: 110,
+    atk: 15,
+    intro: '血狼將軍率軍襲來。'
+  },
+  ghost_scholar: {
+    name: '鬼智師',
+    level: 12,
+    hp: 135,
+    atk: 18,
+    intro: '鬼智師施展幻術。'
+  },
+  boss_shadow: {
+    name: '幻影羅喉',
+    level: 16,
+    hp: 180,
+    atk: 24,
+    intro: '你的心魔具現化。'
+  }
+};
