9308d93b109f3e5c66f6fbb7d7ead9631289058b
kingchen0617 <a0980192456@gmail.com>
2026-04-09 12:15:08 +0800

    Add v4-fixed enemies

---
+++ v4-fixed/data/enemies.js
@@ -0,0 +1,26 @@
+const enemies = {
+  bandit: {
+    name: '荒野掠奪者',
+    level: 3,
+    hp: 60,
+    atk: 10,
+    drop: ['potion_small'],
+    intro: '一群掠奪者試圖圍攻你。'
+  },
+  wolf_general: {
+    name: '血狼將軍',
+    level: 8,
+    hp: 110,
+    atk: 15,
+    drop: ['blade_jidu'],
+    intro: '血狼將軍率軍襲來。'
+  },
+  ghost_scholar: {
+    name: '鬼智師',
+    level: 12,
+    hp: 135,
+    atk: 18,
+    drop: ['ether_small'],
+    intro: '鬼智師施展幻術。'
+  }
+};
