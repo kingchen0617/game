951a1bd7c62bf76ffd39c55cee15a315cffc01a3
kingchen0617 <a0980192456@gmail.com>
2026-04-09 12:14:58 +0800

    Add v4-fixed items data

---
+++ v4-fixed/data/items.js
@@ -0,0 +1,40 @@
+const items = {
+  potion_small: {
+    id: 'potion_small',
+    name: '小還丹',
+    type: 'consumable',
+    desc: '回復 35 點氣血。',
+    effect: { hp: 35 }
+  },
+  ether_small: {
+    id: 'ether_small',
+    name: '凝元散',
+    type: 'consumable',
+    desc: '回復 18 點真元。',
+    effect: { mp: 18 }
+  },
+  blade_jidu: {
+    id: 'blade_jidu',
+    name: '計都殘刃',
+    type: 'weapon',
+    slot: 'weapon',
+    atk: 8,
+    desc: '殘留煞氣的兵刃，增幅攻勢。'
+  },
+  armor_tiandu: {
+    id: 'armor_tiandu',
+    name: '天都戰甲',
+    type: 'armor',
+    slot: 'armor',
+    hp: 30,
+    desc: '舊日武衛戰甲，提升生存力。'
+  },
+  ring_lonepeak: {
+    id: 'ring_lonepeak',
+    name: '孤峰戒',
+    type: 'accessory',
+    slot: 'accessory',
+    mp: 15,
+    desc: '凝心聚神之物，提升真元上限。'
+  }
+};
